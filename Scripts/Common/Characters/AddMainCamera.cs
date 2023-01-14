d807e8a7147aecdd01e8f570960d41b0f9df82d4
mroshaw <oli@mroshaw.co.uk>
2023-01-14 16:20:04 +0000

    Character changes

---
+++ Scripts/Common/Characters/AddMainCamera.cs
@@ -0,0 +1,36 @@
+#if INVECTOR_SHOOTER
+using DaftAppleGames.GameControllers;
+using Invector.vCamera;
+using UnityEngine;
+
+public class AddMainCamera : MonoBehaviour
+{
+
+    private vThirdPersonCamera vCamera;
+    /// <summary>
+    /// Add the Main Camera game object as a child
+    /// </summary>
+    private void Start()
+    {
+        GameObject cameraGameObject = GameController.GetMainCameraGameObject();
+        if (!cameraGameObject)
+        {
+            Debug.LogError("AddMainCamera: Can't find 'MainCamera' game object!!!");
+            return;
+        }
+
+        vCamera = GetComponent<vThirdPersonCamera>();
+
+        if (!vCamera)
+        {
+            Debug.LogError("AddMainCamera: Can't find 'vThirdPersonCamera' component!!!");
+            return;
+        }
+
+        cameraGameObject.transform.SetParent(transform, false);
+        cameraGameObject.transform.localPosition = new Vector3(0, 0, 0);
+        cameraGameObject.transform.localRotation = new Quaternion(0, 0, 0, 0);       
+    }
+
+}
+#endif
---
+++ Scripts/Common/Characters/EyeSync.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+namespace DaftAppleGames.Character
+{
+
+    public class EyeSync : MonoBehaviour
+    {
+        [Header("Blendshapes Config")]
+        public SkinnedMeshRenderer blendShapesSource;
+        public string lookLeftBlendshape;
+        public string lookRightBlendshape;
+        public string lookUpBlendshape;
+        public string lookDownBlendshape;
+
+        private int leftBlendShapeIndex;
+        private int rightBlendShapeIndex;
+        private int upBlendShapeIndex;
+        private int downBlendShapeIndex;
+
+        private Vector3 _lookDir;
+        private float _lookLeftRight;
+        private float _lookUpDown;
+
+        private void Start()
+        {
+            leftBlendShapeIndex = blendShapesSource.sharedMesh.GetBlendShapeIndex(lookLeftBlendshape);
+            rightBlendShapeIndex = blendShapesSource.sharedMesh.GetBlendShapeIndex(lookRightBlendshape);
+            upBlendShapeIndex = blendShapesSource.sharedMesh.GetBlendShapeIndex(lookUpBlendshape);
+            downBlendShapeIndex = blendShapesSource.sharedMesh.GetBlendShapeIndex(lookDownBlendshape);
+
+            Debug.Log($"{leftBlendShapeIndex}, {rightBlendShapeIndex}, {upBlendShapeIndex}, {downBlendShapeIndex}");
+        }
+
+        void Update()
+        {
+            // Move world look rotation to local rotation
+            Quaternion localLookRot = Quaternion.Inverse(transform.rotation); // * Quaternion.Euler(EyesAnim.DeltaVectorClamped);
+
+            // Define look vector
+            _lookDir = localLookRot * Vector3.forward;
+
+            _lookLeftRight = _lookDir.x;
+            _lookUpDown = _lookDir.y;
+
+            // Blendshapes
+            if(_lookLeftRight == 0)
+            {
+                blendShapesSource.SetBlendShapeWeight(leftBlendShapeIndex, 0);
+                blendShapesSource.SetBlendShapeWeight(rightBlendShapeIndex, 0);
+            }
+            else if(_lookLeftRight > 0)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            if (_lookUpDown == 0)
+            {
+                blendShapesSource.SetBlendShapeWeight(upBlendShapeIndex, 0);
+                blendShapesSource.SetBlendShapeWeight(downBlendShapeIndex, 0);
+            }
+            else if (_lookUpDown > 0)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            
+        }
+    }
+}
