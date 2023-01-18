09ac315fd841484fd9c8167ec69cd18458557bee
mroshaw <oli@mroshaw.co.uk>
2023-01-18 15:25:51 +0000

    Added for virtual mouse

---
+++ Scripts/Common/Ui/GamepadCursor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DaftAppleGames.Common.UI
+{
+    public class GamepadCursor : MonoBehaviour
+    {
+ 
+        private void Start()
+        {
+        }
+        void Update()
+        {
+ 
+        }
+    }
+}
