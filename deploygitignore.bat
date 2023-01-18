d6034f23205ee1f33f509bc7fe2d60721f8ae2cc
mroshaw <oli@mroshaw.co.uk>
2023-01-18 15:25:40 +0000

    Added to easily control global gitignore

---
+++ deploygitignore.bat
@@ -0,0 +1,6 @@
+copy .\mastergitignore.txt .\Darskerry-Buildings\Assets\.gitignore /Y
+copy .\mastergitignore.txt .\Darskerry-ControllerAndAI\Assets\.gitignore /Y
+copy .\mastergitignore.txt .\Darskerry-Game\Assets\.gitignore /Y
+copy .\mastergitignore.txt .\Darskerry-Terrain\Assets\.gitignore /Y
+copy .\mastergitignore.txt .\Darskerry-UserInterface\Assets\.gitignore /Y
+pause
